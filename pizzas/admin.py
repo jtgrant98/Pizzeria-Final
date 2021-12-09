5681efbae9da3213c7d913efd6f7ad6cc6df07d3
Josh Grant <Josh_grant1@baylor.edu>
2021-12-09 15:04:51 -0600

    added toppings to admin file

--- pizzas/admin.py
+++ pizzas/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from .models import Toppings
 
 # Register your models here.
 
 from .models import Pizza
 
 admin.site.register(Pizza)
+admin.site.register(Toppings)
